06ab47b9a7f86024abeb2d09518320674bc5d3bd
Kinamd-east <kinamdeast@gmail.com>
2025-07-09 22:50:07 +0100

    Changes

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { usePrivy } from "@privy-io/react-auth";
+import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
 const Profile = () => {
   const { id } = useParams();
@@ -58,9 +114,36 @@ const Profile = () => {
       {ready ? (
         authenticated ? (
           user ? (
-            <div className="flex flex-col p-8">
-              <div className="flex flex-row text-left gap-2 items-center justify-between">
-                <div className="flex flex-row text-left gap-2 items-center">
+            // <div class="flex flex-col p-8">
+            //   <div class="flex flex-row text-left gap-2 items-center justify-between">
+            //     <div class="flex flex-row text-left gap-2 items-center">
+            //       <Avatar>
+            //         <AvatarImage
+            //           src={user?.twitterPfp}
+            //           alt={user?.twitterName}
+            //         />
+            //         <AvatarFallback>{user?.twitterUsername}</AvatarFallback>
+            //       </Avatar>
+            //       <div class="flex flex-col">
+            //         <h1 class="font-bold text-black">{user?.twitterName}</h1>
+            //         <p class="text-gray-500 text-[14px] font-semibold">
+            //           @{user?.twitterUsername}
+            //         </p>
+            //       </div>
+            //     </div>
+            //     <div class="bg-gray-800 text-white rounded-xl p-4 shadow-md">
+            //       <div class="text-center">
+            //         <h1 class="font-bold text-xl md:text-2xl lg:text-3xl mb-2">
+            //           Total $nese Earned
+            //         </h1>
+            //         <p class="font-light text-lg">0</p>
+            //       </div>
+            //     </div>
+            //   </div>
+            // </div>
+            <div className="dashboard-container p-8">
+              <header className="flex justify-between items-center mb-4">
+                <div class="flex flex-row text-left gap-2 items-center">
                   <Avatar>
                     <AvatarImage
                       src={user?.twitterPfp}
@@ -68,13 +151,54 @@ const Profile = () => {
                     />
                     <AvatarFallback>{user?.twitterUsername}</AvatarFallback>
                   </Avatar>
-                  <div className="flex flex-col">
-                    <h1 className="fomnt-bold text-black">
-                      {user?.twitterName}
-                    </h1>
-                    <p className="text-gray-500 text-[14px] font-semibold">
+                  <div class="flex flex-col">
+                    <h1 class="font-bold text-black">{user?.twitterName}</h1>
+                    <p class="text-gray-500 text-[14px] font-semibold">
                       @{user?.twitterUsername}
                     </p>
                   </div>
                 </div>
-                <d
+                <Button className="bg-black cursor-pointer hover:bg-gray-500 transition text-white px-4 py-2 rounded">
+                  Connect Wallet
+                </Button>
+              </header>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="bg-white rounded-lg shadow-md p-4">
+                  <h2 className="font-bold font-mono text-lg">
+                    Total earned $NESE
+                  </h2>
+                  <p className="font-mono font-semibold text-center">100</p>
+                </div>
+                {/*<div className="bg-white rounded-lg shadow-md p-4">
+                  <h2 className="font-bold text-lg font-mono">
+                    Project Watchlist
+                  </h2>
+                  <ul className="flex flex-col gap-1 items-center">
+                    <li className="font-semibold font-mono"> * Openledger</li>
+                    <li className="font-semibold font-mono"> * Bybit</li>
+                    <li className="font-semibold font-mono"> * Bitget</li>
+                  </ul>
+                </div>*/}
+                <div className="bg-white rounded-lg shadow-md p-4">
+                  <h2 className="font-bold text-lg font-mono">
+                    Mayo Followers
+                  </h2>
+                  <p className="font-mono font-bold text-center">150</p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1>Loading...</h1>
+          )
+        ) : (
+          <h1>You have to sign in to access this data</h1>
+        )
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
